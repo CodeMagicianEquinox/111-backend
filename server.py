c3d34032f57e17185457d4cc3407324a12884f92
Tim Terrance <timothy.terrance@sdgku.edu>
2025-10-09 22:35:16 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,175 +1,69 @@
-from flask import Flask, jsonify, request
-import sqlite3
-from datetime import date
+from flask import Flask, render_template, request, session, redirect, jsonify
+import requests
 
 app = Flask(__name__)
 
-DB_NAME = "budget_manager.db"
-
-def init_db():
-    conn = sqlite3.connect(DB_NAME) # Opens a connection to the database file named 'budget_manager.db'
-    cursor = conn.cursor() # Creates a Cursor/Tool that lets us send commands(SELECT, INSERT...) to the database.
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      username TEXT UNIQUE NOT NULL,
-      password TEXT NOT NULL              
-    )
-    """)
-
-    # Expenses Table
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS expenses (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      title TEXT,
-      description TEXT NOT NULL,
-      amount TEXT NOT NULL,
-      date TEXT NOT NULL,
-      category TEXT NOT NULL,
-      user_id INTEGER,
-      FOREIGN KEY(user_id) REFERENCES users(id)
-    )
-    """)
 
 
-    conn.commit() # Save changes to the dabase
-    conn.close() # Close the connection to the database.
+app.secret_key = "mi-secret-key-cohort60"
+
+# http://127.0.0.1:5001/about
+@app.get("/about")
+def about():
+    return render_template("about.html")
 
 
-# @app.route("/api/health", methods=["GET"])
-# def health_check():
-#     return jsonify({"status": "OK"}), HTTPStatus.OK
+@app.get("/contact")
+def contact():
+    name = "Big John" #can send data as a variable
+    return render_template("contact.html", name=name)
 
 
-@app.get("/api/health")
-def health_check():
-   return jsonify({"status": "OK"}), 200
+@app.get("/")
+@app.get("/home")
+@app.get("/index")
+def home():
+  if 'user_id' in session:
+    user_id = session['user_id']
+    response = requests.get(f"http://localhost:5000/api/expenses?user_id={user_id}")
+    expenses = response.json()
+
+    return render_template('home.html', username=session['username'], expenses=expenses)
+  else:
+    return render_template('login.html')
 
 
-# ---------| USER |---------
-@app.post("/api/register")
-def register():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+# http://127.0.0.1:5000/login
+@app.route("/login", methods=["GET", "POST"]) # Should be the exact same url as the backend code
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
 
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
+    username = request.form.get("username")
+    password = request.form.get("password")
 
-    # Insert a new user into DB
-    cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
-    conn.commit() # Save changes to the database
-    conn.close()
+    response = requests.post("http://127.0.0.1:5000/api/login", json={
+        "username": username,
+        "password": password
+    })
 
-    return jsonify({"message": "user registered successfully"}), 201
-
-
-# http://127.0.0.1:5000/api/users/3
-@app.get("/api/users/<int:user_id>")
-def get_user(user_id):
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row # Allows column values to be retrieved by name, row["username"]
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,))
-    row = cursor.fetchone()
-    conn.close()
-
-    if not row:
-        return jsonify({"message": "User not found"}), 404
-
-    return jsonify({"id": row["id"], "username": row["username"]}), 200
-
-
-# http://127.0.0.1:5000/api/users/2
-@app.put("/api/users/<int:user_id>")
-def update_user(user_id):
-  data =request.get_json() #Flask
-  username = data.get("username") #Python
-  password = data.get("password") #Python
-
-  conn = sqlite3.connect(DB_NAME) #Sqlite3
-  cursor = conn.cursor()
-
-# Validate if user exists
-  cursor.execute("SELECT * FROM users WHERE id=?", (user_id,)) #execute(), executes an SQL statement
-  if not cursor.fetchone(): #gets a single row from the result
-        conn.close()
-        return jsonify({'message': "User not found"}), 404
-
-  cursor.execute("UPDATE users SET username=?, password=? WHERE id=?", (username, password, user_id)) #SQL + sqlite3
-  conn.commit()
-  conn.close()
-
-# http://127.0.0.1:5000/api/users/2
-@app.delete("/api/users/<int:user_id>")
-def delete_user(user_id):
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
+    if response.status_code == 200:  
+        data = response.json()
+        session["user_id"] = data["user_id"] # stored as cookie
+        session["username"] = data["username"] # stored as cookie
+        return redirect("home")
+    else:
+        return jsonify({"error": "Login failed"}), 401
     
 
-    # Validate if user exists
-    cursor.execute("SELECT * FROM users WHERE id=?", (user_id,)) #execute(), executes an SQL statement
-    if not cursor.fetchone(): #gets a single row from the result
-        conn.close()
-        return jsonify({'message': "User not found"}), 404
-
-    # Delete user if they exist
-    cursor.execute("DELETE FROM users WHERE id=?", (user_id,))
-    conn.commit()  # save changes to database
-    conn.close()   # close the connection
-   
-    return jsonify({"message": "User deleted successfully"}), 200
-
-
-@app.get("/api/users")
-def get_all_users():
-    conn = sqlite3.connect(DB_NAME)
-    conn.row_factory =sqlite3.Row
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM users")
-    rows = cursor.fetchall()
-    print(f"rows {rows}")
-    conn.close()
-
-    users = []
-    for row in rows:
-      user = {
-        "id": row["id"],
-        "username": row["username"],
-        "password": row["password"]
-    }
-    users.append(user)
-    
-    return jsonify(users), 200
-
-# ----------| EXPENSES |----------
-@app.post("/api/expenses")
-def create_expense():
-    data = request.get_json()
-    title = data.get("title")
-    description = data.get("description")
-    amount = data.get("amount")
-    category = data.get("category")
-    user_id = data.get("user_id")
-    date_str = data.get("date")
-
-    conn = sqlite3.connect(DB_NAME)
-    cursor = conn.cursor()
-
-    cursor.execute("""
-      INSERT INTO expenses (title, description, amount, date, category, user_id) 
-      VALUES (?, ?, ?, ?, ?, ?)
-      """, (title, description, amount, date_str, category, user_id))
-    
-    conn.commit()
-    conn.close()
-
-    return jsonify({"message": "Expense added successfully"}), 201
+@app.route('/logout')
+def logout():
+  session.clear() # Deletes the session
+  return redirect('home')
 
 if __name__ == "__main__":
-    init_db()
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
+
+
+
+
